e94c0effd994a77197a2362d41efb5d50a5f6e84
BooBoster <65795572+BooBoster@users.noreply.github.com>
2023-03-03 17:55:03 +0300

    Add files via upload

---
+++ BankruptcyTask/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BankruptcyTask.DAL.Interfaces;
+using BankruptcyTask.DAL;
+using BankruptcyTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using BankruptcyTask.Service.Interfaces;
+
+namespace BankruptcyTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEstateService _estateService;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IEstateService estateService)
+        {
+            _estateService = estateService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _estateService.GetEstates();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BankruptcyTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankruptcyTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
